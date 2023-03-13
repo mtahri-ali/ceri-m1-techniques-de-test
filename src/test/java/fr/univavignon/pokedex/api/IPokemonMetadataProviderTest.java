f87f479d3b36dae6b16cd3a2586c8c9c54282c1d
etudiant <ali.mtahri@alumni.univ-avignon.fr>
2023-03-13 14:28:45 +0100

    TP3t2

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -0,0 +1,12 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IPokemonMetadataProviderTest {
+
+    @Test
+    public void getPokemonMetadata() {
+    }
+}
