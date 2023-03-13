1300dcf9ffa887981a8a1f78432b5cbd2dd4df91
etudiant <ali.mtahri@alumni.univ-avignon.fr>
2023-03-13 09:47:35 +0100

    TP3t2

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,6 +1,6 @@
 package fr.univavignon.pokedex.api;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
---
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -0,0 +1,67 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javafx.beans.binding.Bindings.when;
+import static org.junit.Assert.*;
+
+public class IPokedexTest {
+    public static List<Pokemon> pokedex1;
+    IPokedex test;
+    IPokemonFactory pokemonFactory1;
+    IPokemonMetadataProvider pokemonMD1;
+
+    @Before
+    public void init() {
+        pokedex1 = new ArrayList<Pokemon>();
+        test = mock(IPokedex.class);
+        pokemonFactory1 = mock(IPokemonFactory.class);
+        pokemonMD1 = mock(IPokemonMetadataProvider.class);
+    }
+
+    @Test
+    public void size() {
+        addPokemon();
+        IPokedex pokedex1 = null;
+        when(test.size()).thenReturn(pokedex1.size());
+        assertEquals(test.size(), 2);
+    }
+
+    @Test
+    public void addPokemon() {
+        //Création instance pokemon1
+        Pokemon pokemon1 = new Pokemon(0, "Bulbizarre" , 126, 126, 90, 613, 64, 4000, 4, 56);
+
+        //Création instance pokemon2
+        Pokemon pokemon2 = new Pokemon(133, "Aquali" , 186, 168, 260, 2729, 202, 5000, 4, 100);
+
+        pokedex1.add(pokemon1);
+        pokedex1.add(pokemon2);
+
+        when(test.addPokemon(pokemon1)).thenReturn(pokemon1.getIndex());
+        when(test.addPokemon(pokemon2)).thenReturn(pokemon2.getIndex());
+
+        assertEquals(test.addPokemon(pokemon1), 0);
+        assertEquals(test.addPokemon(pokemon2), 133);
+    }
+
+
+
+    @Test
+    public void getPokemon() {
+        
+    }
+
+
+
+    @Test
+    public void testGetPokemons() {
+    }
+}
