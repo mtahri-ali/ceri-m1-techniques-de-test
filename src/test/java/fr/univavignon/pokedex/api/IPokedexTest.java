7815c8f4c655332a3317430772c01d3b48c0e87a
etudiant <ali.mtahri@alumni.univ-avignon.fr>
2023-03-13 11:06:20 +0100

    TP3t2

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -3,20 +3,57 @@ package fr.univavignon.pokedex.api;
 
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 
-
+import java.util.Comparator;
 import java.util.List;
 
 
 import static org.junit.Assert.*;
+
 
 public class IPokedexTest {
     public static List<Pokemon> pokedex1;
     IPokedex test;
 
 
-    IPokedex pokedex;
+    IPokedex pokedex = new IPokedex() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public int addPokemon(Pokemon pokemon) {
+            return 0;
+        }
+
+        @Override
+        public Pokemon getPokemon(int id) throws PokedexException {
+            return null;
+        }
+
+        @Override
+        public List<Pokemon> getPokemons() {
+            return null;
+        }
+
+        @Override
+        public List<Pokemon> getPokemons(Comparator<Pokemon> order) {
+            return null;
+        }
+
+        @Override
+        public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
+            return null;
+        }
+
+        @Override
+        public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
+            return null;
+        }
+    };
 
 
     @Test
