b3e5eb036fe47869da6791d9dd416cba03234279
etudiant <ali.mtahri@alumni.univ-avignon.fr>
2023-03-13 09:58:17 +0100

    TP3t2

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -28,20 +28,7 @@ public class IPokedexTest {
 
     @Test
     public void addPokemon() {
-        //Création instance pokemon1
-        Pokemon pokemon1 = new Pokemon(0, "Bulbizarre" , 126, 126, 90, 613, 64, 4000, 4, 56);
 
-        //Création instance pokemon2
-        Pokemon pokemon2 = new Pokemon(133, "Aquali" , 186, 168, 260, 2729, 202, 5000, 4, 100);
-
-        pokedex1.add(pokemon1);
-        pokedex1.add(pokemon2);
-
-        when(test.addPokemon(pokemon1)).thenReturn(pokemon1.getIndex());
-        when(test.addPokemon(pokemon2)).thenReturn(pokemon2.getIndex());
-
-        assertEquals(test.addPokemon(pokemon1), 0);
-        assertEquals(test.addPokemon(pokemon2), 133);
     }
 
 
---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -0,0 +1,12 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IPokemonFactoryTest {
+
+    @Test
+    public void createPokemon() {
+    }
+}
