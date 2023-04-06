f235f25a5fb784b39450c15591b185589a227df7
etudiant <ali.mtahri@alumni.univ-avignon.fr>
2023-04-06 12:05:16 +0200

    IPokedexFactoryTest

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -16,52 +16,22 @@ import static org.junit.jupiter.api.Assertions.*;
 public class IPokedexTest extends TestCase {
 
 
-    IPokedex pokedex;
-    Pokemon bulbizarre;
-    Pokemon aquali;
-    ArrayList<Pokemon> pokemons;
-    IPokedex test;
-    public static List<Pokemon> pokedex1;
+
     @Before
     public void init() {
-        pokedex1 = new ArrayList<Pokemon>();
-        pokedex = Mockito.mock(IPokedex.class);
-        // Initialisation des Pokymons
-        bulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56.0);
-        aquali = new Pokemon(133, "Aquali", 186, 186, 260, 2729, 202, 5000, 4, 100.0);
-       //pokemons = new ArrayList<>(0);
-       // ArrayList<Pokemon> pokemons = null;
 
-        // Ajout des pokymons à la liste
-        pokemons.add(bulbizarre);
-        pokemons.add(aquali);
 
 
     }
     @Test
     void size() {
-        Mockito.doReturn(pokemons.size()).when(pokedex).size();
-        // Verification de la taille de liste est bien 2
-        Assert.assertEquals(2, pokedex.size());
+
     }
 
 
 
     public void testAddPokemon() {
-        //Création instance pokemon1
-        Pokemon pokemon1 = new Pokemon(0, "Bulbizarre" , 126, 126, 90, 613, 64, 4000, 4, 56);
 
-        //Création instance pokemon2
-        Pokemon pokemon2 = new Pokemon(133, "Aquali" , 186, 168, 260, 2729, 202, 5000, 4, 100);
-
-        pokedex1.add(pokemon1);
-        pokedex1.add(pokemon2);
-
-        when(test.addPokemon(pokemon1)).thenReturn(pokemon1.getIndex());
-        when(test.addPokemon(pokemon2)).thenReturn(pokemon2.getIndex());
-
-        assertEquals(test.addPokemon(pokemon1), 0);
-        assertEquals(test.addPokemon(pokemon2), 133);
     }
 
     @Test
